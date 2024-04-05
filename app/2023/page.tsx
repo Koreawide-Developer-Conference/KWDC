d7d6a7f90fd1de400122d6c65bf1e2250ae5904c
Changhyeon Yoon <hyeon120411@gmail.com>
2024-04-06 00:30:09 +0900

    Feat : 2023 페이지는 일단 리다이렉트 하자

--- app/2023/page.tsx
+++ app/2023/page.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function KWDC2023() {
+  redirect("https://kwdc.dev");
   return (
     <main className='w-screen min-h-screen relative flex flex-col items-center'>
       <h1>KWDC 2023</h1>
