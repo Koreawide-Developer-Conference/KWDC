0eabe42cad720121e045cd237835c55e0df61960
Changhyeon Yoon <hyeon120411@gmail.com>
2024-04-06 13:04:37 +0900

    Fix : assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-    output: process.env.NODE_ENV !== "production" ? "standalone" : "export",
-    reactStrictMode: true,
-    trailingSlash: true,
-    assetPrefix: process.env.NODE_ENV !== "production" ? "" : `/KWDC/`,
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/2024",
-                permanent: true,
-            },
-        ];
-    },
+  output: process.env.NODE_ENV !== "production" ? "standalone" : "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/2024",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
