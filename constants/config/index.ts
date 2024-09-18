87092496f864b7b6b00fa9ea2f93d7139c94fbc7
Changhyeon Yoon <yoon@changhyeon.net>
2024-09-19 02:29:09 +0900

    Fix : Prefix

--- constants/config/index.ts
+++ constants/config/index.ts
@@ -1,3 +1,2 @@
-export const repository = "KWDC";
-export const BaseURL = `https://koreawide-developer-conference.github.io/${repository}`;
+export const BaseURL = `https://kwdc.dev`;
 export const prefix = process.env.NODE_ENV !== "production" ? "" : BaseURL;
