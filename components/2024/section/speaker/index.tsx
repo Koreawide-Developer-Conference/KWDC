1587db73e2cce42cb0b9658a37da8c498ae57542
Changhyeon Yoon <yoon@changhyeon.net>
2024-09-18 18:29:55 +0900

    Feat : Speaker Card

--- components/2024/section/speaker/index.tsx
+++ components/2024/section/speaker/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-import { SPEAKERS } from "@/constants";
+import { SPEAKERS, prefix } from "@/constants";
 import { useTranslation } from "@/i18n";
 import { LangProps } from "@/i18n/settings";
 import Icon from "@/public/assets/2024/icons/spaker.svg";
+import Link from "next/link";
 
 export const SpeakerSection: React.FC<LangProps> = async ({ params }) => {
   const { t } = await useTranslation(params.lng, "speaker");
@@ -21,10 +22,23 @@ export const SpeakerSection: React.FC<LangProps> = async ({ params }) => {
           {t("subtitle")} <br />
         </p>
         <div className='grid grid-cols-3 max-md:grid-cols-1 gap-12 py-[75px] w-full'>
-          {SPEAKERS.map((speaker, index) => (
-            <div key={speaker.name} className='bg-brightGreen-100 rounded-xl w-full h-[225px]'>
-              <p>{speaker.name}</p>
-            </div>
+          {Array.from({ length: 1 }).map((_, index) => (
+            <Link href={`/${params.lng}/2024/speaker/${index + 1}`} key={index} className='bg-brightGreen-100 rounded-xl w-full h-[230px] py-4 px-3.5 flex flex-col justify-around'>
+              <div className='flex flex-col'>
+                <p className='text-deepGreen-70 font-SFProDisplay mb-2'>{t(`speaker${index + 1}.category`)}</p>
+                <p className='text-deepGreen-100 font-bold font-SFProDisplay mb-2'>{t(`speaker${index + 1}.title`)}</p>
+                <p className='text-deepGreen-70 font-SFProDisplay'>{t(`speaker${index + 1}.time`)}</p>
+              </div>
+              <div className='py-2 flex items-center gap-x-2'>
+                <img
+                  src={`${prefix}${t(`speaker${index + 1}.img`)}`}
+                  alt={t(`speaker${index + 1}.name`)}
+                  className='w-[60px] h-[60px] object-cover rounded-full'
+                />
+                <p className='text-deepGreen-100 font-bold font-SFProDisplay mb-2'>{t(`speaker${index + 1}.name`)}</p>
+                <p className='text-[#FFA412] font-bold font-SFProDisplay mb-2'>{t(`speaker${index + 1}.nickname`)}</p>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
