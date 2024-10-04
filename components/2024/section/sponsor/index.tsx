e0c2bcbe5daaf5f88fd787ce0c8016b6bc6e9541
Changhyeon Yoon <yoon@changhyeon.net>
2024-10-05 06:36:58 +0900

    Feat : Sponsor

--- components/2024/section/sponsor/index.tsx
+++ components/2024/section/sponsor/index.tsx
@@ -3,18 +3,20 @@ import React from "react";
 
 import { useTranslation } from "@/i18n";
 import { LangProps } from "@/i18n/settings";
-import Diamond from "@/public/assets/2024/icons/diamond.svg";
 import Sponsor from "@/public/assets/2024/icons/sponsor.svg";
+import Datadog from "@/public/assets/2024/logos/datadog.svg";
+import Inflearn from "@/public/assets/2024/logos/inflearn.svg";
+import LottieFiles from "@/public/assets/2024/logos/lottieFiles.svg";
+import RevenueCat from "@/public/assets/2024/logos/revenueCat.svg";
 
 const Card = () => {
-
   return (
     <div className='flex flex-col justify-center items-center'>
-      <div className='mb-5 w-[200px] h-[100px] bg-gray-200'/>
+      <div className='mb-5 w-[200px] h-[100px] bg-gray-200' />
       <p className='text-greenBlack-100 text-xl font-bold'>name</p>
     </div>
-  )
-}
+  );
+};
 export const SponsorSection: React.FC<LangProps> = async ({ params }) => {
   const { t } = await useTranslation(params.lng, "sponsor");
   return (
@@ -24,21 +26,31 @@ export const SponsorSection: React.FC<LangProps> = async ({ params }) => {
     >
       <div className='flex flex-col justify-center items-center w-full max-w-[1440px]'>
         <Sponsor />
-        <p className='text-greenBlack-100 font-bold text-4xl max-3xl:text-2xl mt-5 mb-20'>{t("title")}</p>
+        <p className='text-greenBlack-100 font-bold text-4xl max-3xl:text-2xl mt-5 mb-[100px]'>{t("title")}</p>
 
-        <div className='w-full bg-brightGreen-100 rounded-3xl flex flex-col justify-center items-center py-[50px]'>
-          <Diamond className='mb-5'/>
-          <Card />
+        <div className='w-full rounded-3xl flex flex-col justify-center items-center mb-[100px]'>
+          <p className='text-greenBlack-100 font-semibold text-2xl mt-5 mb-20'>{t("diamond")}</p>
+          <Link href={"https://datadoghq.com"} target='_blank' className='max-md:scale-50'>
+            <Datadog />
+          </Link>
         </div>
-        <div className='w-full bg-brightGreen-60 rounded-3xl grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 py-[100px] my-20'>
-          <Card />
-          <Card />
-          <Card />
+        <div className='w-full rounded-3xl flex flex-col justify-center items-center px-10'>
+          <p className='text-greenBlack-100 font-semibold text-2xl mt-5'>{t("platinum")}</p>
+          <Link href={"https://www.revenuecat.com/"} target='_blank' className='max-md:scale-50'>
+            <RevenueCat />
+          </Link>
         </div>
-        <div className='w-full bg-brightGreen-30 rounded-3xl grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 py-[50px]'>
-          <Card />
-          <Card />
-          <Card />
+        <div className='w-full rounded-3xl flex flex-col justify-center items-center mb-[100px]'>
+          <p className='text-greenBlack-100 font-semibold text-2xl mt-5 mb-20'>{t("gold")}</p>
+          <Link href={"https://lottiefiles.com/"} target='_blank' className='max-md:scale-50'>
+            <LottieFiles />
+          </Link>
+        </div>
+        <div className='w-full rounded-3xl flex flex-col justify-center items-center mb-[100px]'>
+          <p className='text-greenBlack-100 font-semibold text-2xl mt-5 mb-20'>{t("in-kind")}</p>
+          <Link href={"https://www.inflearn.com/"} target='_blank' className='max-md:scale-50'>
+            <Inflearn />
+          </Link>
         </div>
       </div>
     </section>
